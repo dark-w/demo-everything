0f87f8cfb2109317696748a74fab8d467851bc30
Dark Wang <dkfire30@outlook.com>
2021-02-07 21:29:24 +0800

    cpp lambda case
    Signed-off-by: Dark Wang <dkfire30@outlook.com>

---
+++ points/cpp-20/lambda.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+
+class f {
+public:
+    int operator()() {
+        return i++;
+    }
+
+private:
+    int i = 0;
+};
+
+int main(int argc, char **argv) {
+#if 0
+    auto f1 = [i = 0]() {
+        return i++;
+    };
+#else
+    f f1;
+#endif
+
+    std::cout << f1() << std::endl
+        << f1() << std::endl
+        << f1() << std::endl;
+
+    return 0;
+}
+
