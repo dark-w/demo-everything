d8b78aa431d3815f62c415fbd8b1ae53bfb41e40
Dark.Wang <dkfire30@outlook.com>
2023-03-30 12:07:09 +0800

    bezier曲线
    Signed-off-by: Dark.Wang <dkfire30@outlook.com>

---
+++ demo/math/bezier.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import math
+import random
+
+x = []
+y = []
+
+controll_points = [[0, 0], [1, 2], [2, 2], [3, 0]]
+
+def bezier(cps, t: float):
+    ret_point = [0.0, 0.0]
+    cps_x = [row[0] for row in cps]
+    cps_y = [row[1] for row in cps]
+    
+    n = len(cps) - 1
+    for k in range(n + 1): 
+        c = math.comb(n, k) * ((1 - t) ** (n - k)) * (t ** k)
+        ret_point[0] += c * cps_x[k]
+        ret_point[1] += c * cps_y[k]
+
+    return ret_point
+
+for t in range(100):
+    bn = bezier(controll_points, t / 100.0)
+    x.append(bn[0])
+    y.append(bn[1])
+
+plt.scatter(x, y)
+
+plt.xlabel('x-axis')
+plt.ylabel('y-axis')
+plt.title('Scatter Plot Example')
+
+plt.show()
